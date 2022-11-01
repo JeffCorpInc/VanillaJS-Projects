47520f02758c85f370cfd8a372fb3dcce395b220
Mehdi Jaffari <112697321+MedisterJaxx@users.noreply.github.com>
2022-11-01 19:36:23 +0500

    checking workflow

--- P02-Movie Theater/script.js
+++ P02-Movie Theater/script.js
@@ -1,12 +1,11 @@
 const movieSelect = document.getElementById("movie");
-const container = document.querySelector("container");
-const seats = document.querySelector(".seatRows .seat:not(.occupied)");
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".seatRows .seat:not(.occupied)");
 const count = document.getElementById("count");
 const totalPrice = document.getElementById("total");    
-const ticketPrices = +totalPrice.value ;                                         //+ will convert the sring into number
+const ticketPrices = +totalPrice.value ;                                         //+ will convert the string into number
 
 //EventListner On Container class
-container.addEventListener("click",function(event){
-        console.log(event.target);
-    }
-)                                          
+container.addEventListener("click",a=>{
+console.log("clicked",a.target)
+})                                      
