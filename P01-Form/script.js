3d53c57eb2d54d7567070eb950089b2decb23880
Mehdi Jaffari <112697321+MedisterJaxx@users.noreply.github.com>
2022-11-03 20:13:04 +0500

    JS functionalities updated full
    JS code completed

--- P01-Form/script.js
+++ P01-Form/script.js
@@ -1,5 +1,5 @@
-const form = document.getElementById("JS-control");         //we have stored all "input tags" into constant variable 
-const username = document.getElementById("username");       //const , let and var are storing varibales
+const form = document.getElementById("JS-control");                                 //we have stored all "input tags" into constant variable 
+const username = document.getElementById("username");                               //const , let and var are storing varibales
 const email = document.getElementById("email");
 const password1 = document.getElementById("password1");
 const password2 = document.getElementById("password2");
@@ -7,24 +7,24 @@ const password2 = document.getElementById("password2");
 //All Functions
 
 //error function
-function showError(input,message){                          //we just created a func. here we'll link it to the event listner to execute it according       
-        const AFormControl = input.parentElement;           //.parentElement will trigger us to the parent element of the "input tag"
+function showError(input,message){                                                  //we just created a func. here we'll link it to the event listner to execute it according       
+        const AFormControl = input.parentElement;                                   //.parentElement will trigger us to the parent element of the "input tag"
         AFormControl.className = "form-control error";
         const small = AFormControl.querySelector("small");
-        small.innerText = message;                            //innerText and innerHTML are same
+        small.innerText = message;                                                  //innerText and innerHTML are same
 }
 
 //success function
 function showSuccess(input) {
-        const AFormControl = input.parentElement;           //.parentElement will trigger us to the parent element of the "input tag"
+        const AFormControl = input.parentElement;                                   //.parentElement will trigger us to the parent element of the "input tag"
         AFormControl.className = "form-control sucess";
 }
 
 //valid email
-function emailvalid(input) {                                    //regex regular expression
+function emailvalid(input) {                                                        //regex regular expression
         const  re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
-        if (re.test(input.value.trim())){                        //.test is testing our email
-            showSuccess(input);                                  //trim will remove extra spaces etc
+        if (re.test(input.value.trim())){                                            //.test is testing our email
+            showSuccess(input);                                                      //trim will remove extra spaces etc
         }
         else{
             showError(input, `${getfieldID(input)} is not Valid`)
@@ -38,10 +38,10 @@ function PasswordMatch(input1, input2){
 }
 //Fields Checking
 function fieldCheck(Array){
-        Array.forEach(function(input){                           //forEach is a high order array method loop... used with array
+        Array.forEach(function(input){                                              //forEach is a high order array method loop... used with array
 
-            if (input.value === ""){                            //agr input wala dabba khali ho to show error
-                showError(input ,`${getfieldID(input)} is required`);     //it is called template literals .We can use concatenation method as well    
+            if (input.value === ""){                                                //agr input wala dabba khali ho to show error
+                showError(input ,`${getfieldID(input)} is required`);                //it is called template literals .We can use concatenation method as well    
             } else {
                 showSuccess(input);
             }
@@ -71,9 +71,9 @@ function InputLength(input, min , max){
 //event Listener and executioner at "submission"
 form.addEventListener("submit" , function(e){
 
-        e.preventDefault();                                     //preventDefault will stop the form from relaoding
+        e.preventDefault();                                                         //preventDefault will stop the form from relaoding
         //we can use IF/ELSE method as well
-        fieldCheck([username,email,password1,password2]);      //Array 
+        fieldCheck([username,email,password1,password2]);                           //Array 
         InputLength(username,3,10);    
         InputLength(password1,6,20);    
         emailvalid(email);
--- P02-Movie Theater/script.js
+++ P02-Movie Theater/script.js
@@ -5,38 +5,59 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("Movie");                                           //movieSelect me ham "Movie" id ko save kar rhe hn or ticketPrice me ham movieSelect.value ko save kar rhe 
 let ticketPrice = +movieSelect.value;                                                           //+ will convert the string into number
 
-//EventListner On Container class
+populateUI();
+
+//EventListner On """"Container"""" class
 container.addEventListener("click", e=>{
 
         if(e.target.classList.contains("seat") && !e.target.classList.contains("occupied")){    //"classList", list ko show kre gi or "contain" me ham class ko define kare ge to perform task 
            
             e.target.classList.toggle("selected");                                              //2 ways to select - add -  toggle ..toggle will select and deselect but add only add once where remove is used to deselect
             updatingCount();                                                                    //classList me selected name ki class ko add kiya he using toggle 
-            
         }                                                                                      
     }
 )                                      
-//EventListner on Movie select 
+//EventListner on """"Movie"""" Class
 movieSelect.addEventListener("change" , e => {                                                  //change dropdown ke liye use kiya jata he..option change pr hame ticketPrice ko change krna he 
 
         ticketPrice = +e.target.value;                                                          //ticketPrice ko hame update krna he... e.target.value se ham ticketPrice me value get krlenge 
         updatingCount();                                                                        //hame count bhi change krwana tha isliye hamne updatecounting wo wapis call kiya 
+        movieData(e.target.selectedIndex , e.target.value);                                     //selectedIndex is used to return the index of selected item in a dropdown list
     }
 )
 
-//function for counts
+//Functions to show """""movieData"""""
+function movieData (movieIndex , moviePrice){
+   
+    localStorage.setItem("Movie Selected" , movieIndex);
+    localStorage.setItem("Movie Price" , moviePrice);
+}
+
+
+//Pull data from LocalStorage to UI
+function populateUI(){
+
+    const UIupdate =JSON.parse(localStorage.getItem("Seats Selected"));                      //json.parse is a inverse of json.stringify..it returns array
+    
+    if (UIupdate !== null && UIupdate > 0){                                                  //is condition me ham null data nhi lena chah rhe or data 0 se bara ho tab hi return kre values
+                                                                      
+    }
+}
+
+//function for """counts"""
 function updatingCount(){
     
     const seatselected = document.querySelectorAll(".seatRows .seat.selected");                 //ham is syntax me selected seats ko get karna chah rhe hn using querySelector
-    const seatcount = seatselected.length;                                                      //nodeList creat hoti he querySelector se
+    const seatcount = seatselected.length;                                                      //nodeList create hoti he querySelector se
     
-    const seatIndex = [...seatselected].map(seat=>{                                            //convering seatSelected NodeList into Array...spread(...)opreator
+    const seatIndex = [...seatselected].map(seat=>{                                            //converting seatSelected NodeList into Array...spread(...)opreator
 
         return [...seats].indexOf(seat)                                                         //ham seats ko array me return karwa rhe hn 
     })                                                                                          //map is a higher order array method..It execute function on array and also return array                                   
     
+    localStorage.setItem("Seats Selected" , JSON.stringify(seatIndex));                          //"setItem(key, value)" se ham data local storage me save karwate hn.....JSON.stringify array ko string me change krta...ham seatIndex ko string me change kr rhe 
+
     count.innerText = seatcount;                                                                 //count.innerText me pehle 0 tha laikin hamne uski value ko seatcount se change krdiya...jiski waja se count me change aya 
     total.innerText = ticketPrice * seatcount;
 }
 
-//
