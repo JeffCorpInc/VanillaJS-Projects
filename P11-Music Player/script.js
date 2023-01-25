5264dc400a56c86c775b6565b6d3a0a1068f847e
Mehdi Jaffari <112697321+MedisterJaxx@users.noreply.github.com>
2023-01-25 16:08:30 +0500

    Project 11 JS updated
    JS updated and all the functionality added

--- P11-Music Player/script.js
+++ P11-Music Player/script.js
@@ -6,16 +6,15 @@ const audioBox = document.getElementById("audio");
 const progressCont = document.getElementById("progress-cnt");
 const progressBar = document.getElementById("progress");
 const title = document.getElementById("title");
-const coverpic = document.getElementById("cover");
+const coverpic = document.getElementById("cover-pic");
 
 //List/Array of songs 
 const songList = ["DJ DENZ-The Rooster"   ,
                   "Future - Mask off"     ,
                   "Hans Zimmer - Time"    ,
-                  "Kisma - Fingertips "   ,
+                  "Kisma - Fingertips"   ,
                   "LXST CXNTURY - ODIUM"  ,
-                  "SICKICK - Lana Del Rey"
-                                            ];
+                  "SICKICK - Lana Del Rey"];
 
 // track which song is currently playing
 let currentSong = 1;
@@ -24,49 +23,107 @@ let currentSong = 1;
 
 // function to load song to DOM
 
-function loadSong(song){                                                //function banaya jisme hamne audio,cover or title ko update kiya... or us function
+function loadSong(s){                                                            //function banaya jisme hamne audio,cover or title ko update kiya... or us function
     
-    title.innerText = song;                                            // ko call krke songlist se link kiya or currentSong array ko link kiya.
-    audioBox.src = `musics/${song}.mp3`;
-    coverpic.src = `images/${song}.jpg`;
+    title.innerText = s;                                                        // ko call krke songlist se link kiya or currentSong array ko link kiya.
+    audioBox.src = `musics/${s}.mp3`;
+    coverpic.src = `images/${s}.jpg`;
 }
 
+// initial song load
 loadSong(songList[currentSong]);
-// // initial song load
-// loadSong(songList[currentSong]);
 
-// //funtions to playSong
-// function playSong() {
+//funtions to playSong
+function playSong(){
     
-//     musicCont.classList.add("play");                                    //musicCont me play ki class add kardi
-//     playButton.querySelector("i.fas").classList.remove("fa-play");      //html me fa-play ko remove krke fa-pause ko add karwaya 
-//     playButton.querySelector("i.fas").classList.add("fa-pause");
+    musicCont.classList.add("play");                                           //musicCont me play ki class add kardi
+    playButton.querySelector("i.fa-solid").classList.remove("fa-play");         //html me fa-play ko remove krke fa-pause ko add karwaya 
+    playButton.querySelector("i.fa-solid").classList.add("fa-pause");
 
-//     audio.play();
-// }
+    audio.play();
+}
 
-// //funtions to pauseSong
-// function pauseSong() {
+//funtions to pauseSong
+function pauseSong(){
     
-//     musicCont.classList.remove("play");                                    //musicCont me play ki class add kardi
-//     playButton.querySelector("i.fas").classList.add("fa-play");      //html me fa-play ko remove krke fa-pause ko add karwaya 
-//     playButton.querySelector("i.fas").classList.remove("fa-pause");
+    musicCont.classList.remove("play");                                            //musicCont me play ki class add kardi
+    playButton.querySelector("i.fa-solid").classList.add("fa-play");                //html me fa-play ko remove krke fa-pause ko add karwaya 
+    playButton.querySelector("i.fa-solid").classList.remove("fa-pause");
 
-//     audio.pause();
-// }
+    audio.pause();
+}
+
+//funtions to previous Song
+function prevSong(){
+    
+    currentSong--;                                                                      //hamne currentsong ki value me -1 karadiya            
+    if (currentSong < 0 ) {                                                             //or condition di ke songlist ki total length me 1 minus karate rho
+        currentSong = songList.length - 1;
+    }
+    
+    loadSong(songList[currentSong]);                                                    //loadsong karadiya take UI update ho audio or image ki 
+    playSong();                                                                         //playsong function call kardiya take prevsong press pr play ho
+}
+
+//funtions to mext Song
+function nextSong(){
+
+    currentSong++;
+
+    if (currentSong > songList.length - 1) {
+        currentSong = 0;
+    }
+
+    loadSong(songList[currentSong]);
+    playSong();
+}
+
+//funtions to update progress bar
+function updateProgress(e){
+    
+    const {currentTime, duration} = e.srcElement;                       //event premeter ki ek property he srcElement jis se ham timeupdate me se kch data lenge
+    const progressprecentage = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressprecentage}%`;
+}
+//funtions to drag progress bar
+function dragProgress(e){
+    const progressWidth = this.clientWidth;                             //"this" use hota he agr hame ksi bhi jaga/container pr click karna ho 
+    const offsetXvalue = e.offsetX;                                     //offsetX start se ksi bhi point ki value batata he
+    const duration = audioBox.duration;
+    audioBox.currentTime = (offsetXvalue / progressWidth) * duration;
+}
+
+// initial song load
+loadSong(songList[currentSong]);
 
 
-// //EVENTLISTENERS
 
-// // adding eventlisteners
-// // 1- Play button 
-// playButton.addEventListener("click", () => {
 
-//         const isPlaying = musicCont.classList.contains("play");         // hamne ek const banaya, agr musicCont me "play" ki class he to value true return kare
-//         if (isPlaying){
-//             pauseSong();
-//         } else {
-//             playSong();
-//         }
-//     }
-// )
+//EVENTLISTENERS
+
+// adding eventlisteners
+// 1- Play button 
+playButton.addEventListener("click", () => {
+
+        const isPlaying = musicCont.classList.contains("play");         // hamne ek const banaya, agr musicCont me "play" ki class he to value true return kare
+        if (isPlaying){
+            pauseSong();
+        } else {
+            playSong();
+        }
+    }
+)
+// 1- Previous button 
+previousButton.addEventListener("click" , prevSong);
+
+// 2- Next button 
+nextButton.addEventListener('click', nextSong);
+
+// 3- Updated Progress
+audioBox.addEventListener("timeupdate", updateProgress)
+
+// 4- drag Progress
+progressCont.addEventListener("click",dragProgress)
+
+//automatically play next song 
+audioBox.addEventListener("ended", nextSong);
