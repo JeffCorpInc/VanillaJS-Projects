51cc1d9ff06e724f5ce18fb479a8d39ef55bd87d
Mehdi Jaffari <112697321+MedisterJaxx@users.noreply.github.com>
2023-03-18 22:07:45 +0500

    Javascript Updated
    Whole project files are updated and completed

--- P14-Web Speech API/script.js
+++ P14-Web Speech API/script.js
@@ -3,11 +3,15 @@
 const toggleBtn = document.getElementById("toggle-btn");
 const toggledWindow = document.getElementById("custom-text");
 const closeBtn = document.getElementById("close-div");
-const selectVoices = document.getElementById("voices");
+const voiceSelect = document.getElementById("voiceSelect");
 const customText = document.getElementById("text-area");
 const readBtn = document.getElementById("read-btn");
 const main = document.getElementById("main")
 
+
+// Arrays
+
+// Array 1 
 const imgData = [
     {
         image: './images/Angry.jpg',
@@ -31,11 +35,19 @@ const imgData = [
     },
 ];
 
+
+// Array 2
+// array for voices which we will get from API
+let vioces = [];
+
 // Creating boxed for every object in the array using ForEach method
 imgData.forEach(creatBoxes);
 
+
+
 // Functions
-// Funtion to creat boxes
+
+//1 Funtion to creat boxes
 function creatBoxes(dataOBJ){
     
     //Creating Div for the image grid
@@ -51,6 +63,82 @@ function creatBoxes(dataOBJ){
     <p class="imageInfo">${text}</p>
 
     `
+    //add event for speaking test
+
+    box.addEventListener('click', ()=>{
+        setMessage(text);
+        speakText();
+    })
+
     // add new box to dom
     main.appendChild(box);
 }
+
+// initializze speech synthesis
+const message = new SpeechSynthesisUtterance();
+
+// 2 Function to get voice API data and update it to the array
+function populateVoiceList(){
+    if (typeof speechSynthesis === "undefined") {
+      return;
+    }
+  
+    const voices = speechSynthesis.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      document.getElementById("voiceSelect").appendChild(option);
+    }
+}
+  
+populateVoiceList();
+if (typeof speechSynthesis !== "undefined" && speechSynthesis.onvoiceschanged !== undefined){
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+};
+
+// 3 Set message for speech synthesis
+function setMessage(text){
+    message.text = text;
+}
+
+// 4 function to speak the text
+function speakText(){
+    speechSynthesis.speak(message);
+}
+
+// // 5 Function to change the new voice
+// function setVoice(e){
+//     message.voice
+// }
+
+// EventListeners
+
+
+// 1 toggle button -- custom text window popup
+toggleBtn.addEventListener('click',() =>{
+    toggledWindow.classList.add('show');
+})
+
+// 2 toggled window close button
+closeBtn.addEventListener('click', ()=>{
+    toggledWindow.classList.remove('show');
+})
+
+// 3 eventlistener to change voice
+speechSynthesis.addEventListener('voiceschanged', populateVoiceList)
+// voiceSelect.addEventListener('change', setVoice)
+
+// 4 EventListenere for custon text
+readBtn.addEventListener('click', ()=>{
+    
+    setMessage(customText.value);
+    speakText();
+})
