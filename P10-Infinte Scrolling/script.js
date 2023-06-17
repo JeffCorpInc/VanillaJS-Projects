1f67e167d85935e0dcddef84716e6886895fe959
Mehdi Jaffari <112697321+JeffCorpInc@users.noreply.github.com>
2023-06-17 17:33:31 +0500

    Loader not working

--- P10-Infinte Scrolling/script.js
+++ P10-Infinte Scrolling/script.js
@@ -2,11 +2,12 @@ const search = document.getElementById("filter");
 const post_container = document.getElementById("post-container");
 const loader1 = document.getElementById("loader");
 
-let limit=5;
+let limit=20;
 let page=1;
 
 // get data fromk the api
 async function getdata(){
+
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
     const data = await res.json();                                                                            //fetched data in result...res ka data json me store karke return kara lenge  
     return data;    
@@ -16,8 +17,8 @@ async function getdata(){
 async function showdata(){
     const posts = await getdata();
     posts.forEach( post => {
-            const postElement = document.createElement("div");
-            postElement.classList.add("post");
+            const postElement = document.createElement('div');
+            postElement.classList.add('post');
             postElement.innerHTML = `
             <div class="number">${post.id}</div>
             <div class="post-info">
@@ -29,44 +30,53 @@ async function showdata(){
         }
     );
 }
-showdata();
+
 
 //function to show loader 
 function showloader(){
+
     loader1.classList.add('show');
 
     setTimeout( () => {
-        loader1.classList.remove("show");
+
+        loader1.classList.remove('show');
 
         setTimeout( ()=> {
             page++;
             showdata();
-        } , 250)
+        } , 300)
 
     } , 1000);
 }
 
 //function to filter post 
 function filterposts(e){
+
     const filterterm = e.target.value.toUpperCase();
-    const allposts = document.querySelectorAll(".post");                            //queryselector se hamne sare node get krliye of const me save krliya
+    const allposts = document.querySelectorAll('.post');                            //queryselector se hamne sare node get krliye of const me save krliya
     allposts.forEach( post => {                                                     //forEach lagakr hamne sare div jinme pos-title or post-body ki class he wo save karle const me 
+        
         const title = post.querySelector(".post-title").innerText.toUpperCase();
         const body = post.querySelector(".post-body").innerText.toUpperCase();
 
-        if (title.indexOf(filterterm) > -1 || body.indexOf(filterterm) > -1){        //indexOf dekhe ga ke filterterm me "title" he ya nhi or value bari ho -1 se to function apply kre  
-            post.style.display = "flex";
+        if(title.indexOf(filterterm) > -1 || body.indexOf(filterterm) > -1){        //indexOf dekhe ga ke filterterm me "title" he ya nhi or value bari ho -1 se to function apply kre  
+            post.style.display = 'flex';
         } else {
-            post.style.display = "none";
+            post.style.display = 'none';
         }
     })   
 }
 
+showdata();
+
 //event handler 
 //1- event handler for page scrolling 
-window.addEventListener("scroll", () => {
-        const { scrollTop , scrollHeight , clientHeight } = document.documentElement;
-        if( scrollTop + clientHeight === scrollHeight - 75 ){
+window.addEventListener('scroll', () => {
+
+        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+        if (scrollTop + clientHeight === scrollHeight){
+
             showloader();
         }
     }
